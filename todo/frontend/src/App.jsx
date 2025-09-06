4d59c1c183fb78a9f2f3693747b361c0eb43841f
Jaime <jaiimecast04@gmail.com>
2025-09-06 15:59:55 -0500

    ch6 initial commit

---
+++ todo/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl text-center text-gray-900 p-4'>Todo List</h1>
+      <ul>
+
+      </ul>
+    </div>
+  )
+}
+
+export default App
